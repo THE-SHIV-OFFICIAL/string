f6b7741e75d3b8e9fa85f4a4a6bf242ac3c6c47a
THE SHIV <shivofficial004@gmail.com>
2026-06-12 14:18:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID", "27383453"))
 API_HASH = getenv("API_HASH", "4c246fb0c649477cc2e79b6a178ddfaa")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_TOKEN = getenv("BOT_TOKEN")
 
 OWNER_ID = int(getenv("OWNER_ID", "8418584090"))
-MONGO_DB_URI = getenv("MONGO_DB_URI", "")
+MONGO_DB_URI = getenv("MONGO_DB_URI")
 MUST_JOIN = getenv("MUST_JOIN", "betabot_hub")
 
 # ✅ Added your new images to the list
